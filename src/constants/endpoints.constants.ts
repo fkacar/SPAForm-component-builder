ad8343e6298c0d391e581fdca6eaa28cb7d11d56
Fatih Kaçar <info@fatihkacar.com>
2022-09-23 18:06:13 +0300

    fix(api): api base url build error and add todo

--- src/constants/endpoints.constants.ts
+++ src/constants/endpoints.constants.ts
@@ -1,5 +1,6 @@
+// TODO: Add conditional env variable based value
 // export const apiBaseUrl = 'http://localhost:3001/api/v1/'
-export const apiBaseUrl = 'https://formactor-api.vercel.app//api/v1/'
+export const apiBaseUrl = 'https://formactor-api.vercel.app/api/v1/'
 
 export default {
     formData: {
